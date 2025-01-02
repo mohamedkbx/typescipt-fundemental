662442fd8c1cfc62af4c6b5b98c0921b693fb2bb
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2025-01-02 03:11:48 +0200

    feat: variable and inferenced variables

---
+++ values-and-variables/variables/variable-declaration-and-inference.ts
@@ -0,0 +1,5 @@
+// variable declaration and inference
+let temperature = 5; //inference , typescript has inferred that this is a number
+
+// temperature ='momo'; //error, we cannot assign a string to a number
+const humidity = 10; //literal type => we cannot change the value of humidity and cant re assign it bc of const
